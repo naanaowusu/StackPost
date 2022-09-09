f818dc09f2d901c2885faf8f82e7cb8552838d7a
Nana Appiah <appiahnana41@gmail.com>
2022-09-09 15:55:51 +0500

    Upload Post class and main program file

---
+++ Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StackOverflowPost_App
+{
+    public class Post 
+    {
+        public string  Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Timecreated { get; set; }
+        public int Votes { get; set; }
+
+        public void UpVote() 
+        {
+            Votes++;
+
+        }
+        public void DownVote() 
+        {
+            Votes--;
+        }
+
+        public void DisplayPost() 
+        {
+            Console.WriteLine(Title);
+            Console.WriteLine();
+
+            Console.WriteLine("The date Posted:" + Timecreated.ToLongDateString());
+            Console.WriteLine();
+
+            Console.WriteLine(Description);
+            Console.WriteLine();
+            Console.WriteLine("The vote is: " + Votes);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StackOverflowPost_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var showpost = new Post();
+            showpost.Title = "How to upload a file to Github";
+            showpost.Description = "I have been trying to upload my work on Github\nI have some problems\nCan anyone show to do this.";
+            showpost.Timecreated = new DateTime(2022,9,9);
+
+            showpost.UpVote();
+            showpost.UpVote();
+            showpost.UpVote();
+            showpost.UpVote();
+            showpost.UpVote();
+            
+
+            showpost.DownVote();
+            showpost.DownVote();
+
+            showpost.DisplayPost();
+                    
+
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
